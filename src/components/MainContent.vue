c948dd6f03186a9079a7f87bf8a66d50668c5ee6
lesliefish <yuleics@outlook.com>
2021-07-15 20:30:29 +0800

    add files

---
+++ src/components/MainContent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "MainContent",
+};
+</script>
+
+<style scoped>
+div {
+  width: 70%;
+  margin: auto;
+}
+</style>
--- src/views/Ui/Ui.vue
+++ src/views/Ui/Ui.vue
@@ -1,22 +1,25 @@
 <template>
-    <div>ui</div>
+  <div>
+    <main-content></main-content>
+  </div>
 </template>
 
 <script>
+import MainContent from "../../components/MainContent.vue";
 export default {
   name: "Ui",
   props: {},
+  components: {
+    MainContent,
+  },
   data: function () {
     return {
-      title: "Vue.js 实践"
+      title: "Vue.js 实践",
     };
-  }
+  },
 };
 </script>
 
 
 <style scoped>
-    div {
-        background-color: aquamarine;
-    }
 </style>
